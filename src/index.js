319ca8ad5d5d7da7a8e9db1a5c8530481e1ff9f4
Raman Liakovich <2038634@gmail.com>
2020-03-19 00:00:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,21 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let str = expr.match(/.{1,10}/g).join(' ').replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-').replace(/[**********]/g, '');
+    let words = str.split('  ')
+    let letters = words.map((w) => w.split(' ')); 
+    let decoded = [];
+    
+    for (let  i = 0; i < letters.length; i++) {
+        decoded[i] = [];
+        for (let j = 0; j < letters[i].length; j++) {
+            if (MORSE_TABLE[letters[i][j]]) {
+                decoded[i].push( MORSE_TABLE[letters[i][j]] );
+            }
+        }
+    } 
+    return decoded.map(arr => arr.join('')).join(' ');
+
 }
 
 module.exports = {
